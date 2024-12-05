3d4e2f4d5e6f9cdb595003e52a8ea3f03329ffa1
kathsup <katherinencarvallo22@gmail.com>
2024-12-05 09:34:01 -0600

    Add files via upload

---
+++ fantasma/src/fantasma/ghost.java
@@ -0,0 +1,14 @@
+package fantasma;
+
+import java.util.Scanner;
+
+
+public class ghost {
+    public static void main(String[]args){
+     ghostGame.menuInicio();  // Llamar al menú de inicio
+    
+    }
+    }
+
+
+
